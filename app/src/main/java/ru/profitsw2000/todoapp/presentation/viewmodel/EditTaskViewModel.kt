f8c3e26ceabd79db249647a8456f57bdffa2cb5e
profitsw2000 <profitsw2000@gmail.com>
2025-07-03 22:17:26 +0300

    Сделана EditTaskViewModel для экрана редактирования задания.

---
+++ app/src/main/java/ru/profitsw2000/todoapp/presentation/viewmodel/EditTaskViewModel.kt
@@ -0,0 +1,40 @@
+package ru.profitsw2000.todoapp.presentation.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import ru.profitsw2000.todoapp.data.domain.TasksRepository
+import ru.profitsw2000.todoapp.data.room.model.TaskModel
+import ru.profitsw2000.todoapp.data.state.TaskCreateState
+
+class EditTaskViewModel(
+    private val tasksRepository: TasksRepository
+) : ViewModel() {
+
+    private val _tasksLiveData = MutableLiveData<TaskCreateState>()
+    val tasksLiveData: LiveData<TaskCreateState> by this::_tasksLiveData
+
+    fun editTask(taskModel: TaskModel) {
+        _tasksLiveData.value = TaskCreateState.Loading
+        viewModelScope.launch {
+            _tasksLiveData.value = updateTask(taskModel)
+        }
+    }
+
+    private suspend fun updateTask(taskModel: TaskModel): TaskCreateState {
+        val deferred: Deferred<TaskCreateState> = viewModelScope.async{
+            try {
+                tasksRepository.updateTask(taskModel)
+                TaskCreateState.Success
+            } catch (exception: Exception) {
+                val message = exception.message ?: ""
+                TaskCreateState.Error(message)
+            }
+        }
+        return deferred.await()
+    }
+}
