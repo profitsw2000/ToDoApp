17710d330b3a28d929aeaeded4b45a1680ec3a2c
profitsw2000 <profitsw2000@gmail.com>
2025-07-24 21:48:27 +0300

    Сделан запрет поддержки горизонтального отображения приложения и изменён размер текста с заданием.

--- app/src/main/java/ru/profitsw2000/todoapp/presentation/MainActivity.kt
+++ app/src/main/java/ru/profitsw2000/todoapp/presentation/MainActivity.kt
@@ -1,9 +1,11 @@
 package ru.profitsw2000.todoapp.presentation
 
-import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import ru.profitsw2000.todoapp.R
 import ru.profitsw2000.todoapp.presentation.view.CreateTaskFragment
 import ru.profitsw2000.todoapp.presentation.view.EditTaskFragment
@@ -19,7 +21,13 @@ class MainActivity : AppCompatActivity(), Controller {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
         openMainFragment()
     }
 
